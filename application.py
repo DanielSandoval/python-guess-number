c579f1b855ef24e88d750ce06ba028cd4a7ee751
Daniel Sandoval <danielsandoval182@gmail.com>
2015-04-21 16:51:04 -0600

    Comprueba se el numero ingresado es mayor al numero generado

--- application.py
+++ application.py
@@ -1,4 +1,7 @@
 # Aqui escribe tu codigo
 import random
 numero_random = random.randrange(1,21)
+numero_ingresado = int(raw_input("Adivina el numero entre 1 y 20: "))
+if numero_ingresado > numero_random:
+	print "you guessed to high, please try again"
 print numero_random
