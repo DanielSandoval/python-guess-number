0603e1bd6a42bf255c26c45e46138f0fcd64b1d7
Daniel Sandoval <danielsandoval182@gmail.com>
2015-04-22 11:57:38 -0600

    Solo se puede 4 turnos

--- application.py
+++ application.py
@@ -2,13 +2,22 @@
 import random
 
 numero_random = random.randrange(1,21)
-numero_ingresado = int(raw_input("Adivina el numero entre 1 y 20: "))
 
-if numero_ingresado > numero_random:
-	print "you guessed to high, please try again"
-elif numero_ingresado < numero_random:
-	print "you guessed to low, please try again"
-elif numero_ingresado == numero_random:
-	print "you win!"
+turno = 4
+for turnoNumero in range(1,turno+1):
+	print "Vas en el turno:", turnoNumero
+	if turnoNumero < 5:
+		numero_ingresado = int(raw_input("Adivina el numero entre 1 y 20: "))
+		if numero_ingresado == numero_random:
+			print "you win!"
+			break
+		elif numero_ingresado != numero_random and turnoNumero < 5:
+			if numero_ingresado > numero_random:
+				print "you guessed to high, please try again"
+			elif numero_ingresado < numero_random:
+				print "you guessed to low, please try again"
+			turnoNumero += 1
+		if turnoNumero == 5:
+				print "Game Over"
 
-print numero_random
+print "Este era el numero generado:", numero_random
